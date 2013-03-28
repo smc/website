d649ea9e168bc1d2452fb6088dc647aa6eda3cdd
Jishnu Mohan <jishnu7@gmail.com>
2013-03-29 01:15:26 +0530

    language changing should work now.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
     'use strict';
-    $.i18n.debug = true;
     var i18n = $.i18n();
 
-    $("#homepage").html(_.template($('#template-homepage').html()));
+    function loadpage() {
+        var language = $('option:selected').val() || 'en';
+        i18n.locale = language;
+        $("#homepage").html(_.template($('#template-homepage').html()));
+        $('.language').on('change', loadpage);
+    }
+    loadpage();
 } );
