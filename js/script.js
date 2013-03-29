b7a886b424d105a41e46f31d1484ff67e444996a
Jishnu Mohan <jishnu7@gmail.com>
2013-03-29 17:57:36 +0530

    revamp language selection

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 $(document).ready(function(){
-    'use strict';
     var i18n = $.i18n();
 
     function loadpage() {
-        var cls = $('.language'),
-            language = ($(this).attr("id"))?$(this).attr("id"):"en";
+        var obj = $(this),
+            language = $(this).data("language") || "en";
         i18n.locale = language;
         $("#homepage").html(_.template($('#template-homepage').html()));
-        cls.on('click', loadpage);
-        $(this).unbind("click");
-        }
+    }
     loadpage();
+    $('.language').on('click', loadpage);
 } );
