4b26c8eeda5d45529c053f32f2ea87f7ebe3b716
Balasankar C <balasankarc@autistici.org>
2016-07-29 19:12:38 +0530

    Handle <language>-<country> format

--- js/script.js
+++ js/script.js
@@ -2,8 +2,14 @@ $(document).ready(function(){
     var i18n = $.i18n();
 
     function loadpage() {
+        var browserLanguage = navigator.language;
+        if(browserLanguage.contains('-'))
+        {
+            var position = browserLanguage.indexOf('-');
+            browserLanguage = browserLanguage.substring(0, position);
+        }
         var obj = $(this),
-        language = $(this).data("language") || navigator.language || "en";
+        language = $(this).data("language") || browserLanguage || "en";
         i18n.locale = language;
         $("#homepage").html(_.template($('#template-homepage').html()));
     }
