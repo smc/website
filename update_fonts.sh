303c15b19d44553cbdc03da7fb285bb310b53d2a
Balasankar "Balu" C <balasankar@gitlab.com>
2018-10-06 19:22:57 +0530

    Make URLs configurable

--- update_fonts.sh
+++ update_fonts.sh
@@ -1,16 +1,23 @@
+#!/bin/bash
+
+FONTS_URL=${FONTS_URL:-"smc/fonts"}
+# Convert slash to %2F
+FONTS_SLUG=${FONTS_URL/\//%2F}
+
 mkdir -p downloads/fonts
 cd downloads/fonts || exit
 fonts=(anjalioldlipi rachana meera manjari raghumalayalamsans dyuthi keraleeyam uroob chilanka karumbi)
 for font in "${fonts[@]}"; do
         mkdir "$font"
         cd "$font" || exit
-        version=$(curl -sSL "https://gitlab.com/api/v4/projects/smc%2Ffonts%2F${font}/repository/tags"| jq '.[0]["name"]' | tr -d '"')
-        wget "https://gitlab.com/smc/fonts/${font}/-/jobs/artifacts/${version}/download?job=build-tag" -O artifacts.zip
-        unzip artifacts.zip
+        version=$(curl -sSL "https://gitlab.com/api/v4/projects/${FONTS_SLUG}%2F${font}/repository/tags"| jq '.[].name' | grep -i version | sort -V | tail -1 | tr -d '"')
+        echo "$font : $version"
+        wget -q "https://gitlab.com/${FONTS_URL}/${font}/-/jobs/artifacts/${version}/download?job=build-tag" -O artifacts.zip
+        unzip -q artifacts.zip
         rm artifacts.zip
         mv build/* .
         rm -rf build
         cd ../ || exit
 done
 
-zip -r fonts-smc.zip . -i "*.ttf" -j
+zip -qr fonts-smc.zip . -i "*.ttf" -j
